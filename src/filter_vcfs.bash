4904454483c93bbbbab8856f27b22ef0b7ac9ea0
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-21 13:38:36 -0400

    Test filtering using tabix.
    A file can also be specified giving the genomic coordinates for the filtering.

---
+++ src/filter_vcfs.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
+tabix ${vcf} chr15:596055-39960550
