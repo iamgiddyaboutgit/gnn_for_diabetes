3ff548cf523a62dd4f0bb1428f18eca83af57b5b
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-21 14:23:24 -0400

    Extract from VCF using BED

--- src/filter_vcfs.bash
+++ src/filter_vcfs.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
-tabix ${vcf} chr15:596055-39960550
+regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
+tabix ${vcf} --regions ${regions}
--- src/get_pathway_data.py
+++ src/get_pathway_data.py
@@ -2,5 +2,5 @@
 
 import pywikipathways as pwpw
 
-# print(pwpw.get_pathway_info(pathway="WP623"))
-print(pwpw.get_xref_list(pathway="WP623", system_code="L"))
+print(pwpw.get_pathway_info(pathway="WP623"))
+# print(pwpw.get_xref_list(pathway="WP623", system_code="L"))
