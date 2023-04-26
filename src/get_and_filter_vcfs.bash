85a7d388d613d460114eeced1e67e4b72859b870
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-26 02:39:02 -0400

    Add aws s3 sync command.

--- src/get_and_filter_vcfs.bash
+++ src/get_and_filter_vcfs.bash
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+aws s3 sync \
+    --no-sign-request \
+    --exclude="*" \
+    --include="*.hard-filtered.vcf.gz*" \
+    s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/HG00096/ .
+
 vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
 regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
 # Note that tabix produces something that is no longer compressed.
