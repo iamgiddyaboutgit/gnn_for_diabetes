3a3563d87c4387c9cfa30183becc940620f5742f
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-26 03:12:22 -0400

    Rewrite aws s3 sync command using variables.
    Next step: figure out how to work with slashes in directories

--- src/get_and_filter_vcfs.bash
+++ src/get_and_filter_vcfs.bash
@@ -2,6 +2,7 @@
 
 local_path_to_sync="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based"
 # https://questhenkart.medium.com/looping-through-an-s3-bucket-and-performing-actions-using-the-aws-cli-and-bash-61394a91af89
+# Note that origin has a trailing slash because aws s3 likes that.
 origin="s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/"
 for remote_path in $(aws s3 ls --no-sign-request "${origin}");
 do
@@ -13,13 +14,13 @@ do
     then
         # double_modified_remote_path will not have PRE
         double_modified_remote_path="${modified_remote_path}"
-
+        
 done
-# aws s3 sync \
-#     --no-sign-request \
-#     --exclude="*" \
-#     --include="*.hard-filtered.vcf.gz*" \
-#     s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/ "${path_to_sync}"
+aws s3 sync \
+    --no-sign-request \
+    --exclude="*" \
+    --include="*.hard-filtered.vcf.gz*" \
+    ${origin}${double_modified_remote_path} "${local_path_to_sync}"
 
 # vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
 # regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
