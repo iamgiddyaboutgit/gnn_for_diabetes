ad722f71e1e348fb1e4b74d32e1fc83b28e4731e
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-26 02:19:27 -0400

    Add --separate-regions option to tabix command.
    This option allows for the VCF to be separated by the regions
    that were specified.  This allows you to see how your two files
    are related in a one-to-many structure between regions and variants.

--- src/filter_vcfs.bash
+++ src/filter_vcfs.bash
@@ -2,4 +2,5 @@
 
 vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
 regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
-tabix ${vcf} --regions ${regions}
+# Note that tabix produces something that is no longer compressed.
+tabix ${vcf} --regions ${regions} --separate-regions > "/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data/HG00096.hard-filtered.sliced.vcf"
