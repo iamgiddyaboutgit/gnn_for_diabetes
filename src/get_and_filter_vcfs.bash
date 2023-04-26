0c85771a4b4e60ca6acbfece6aff22d0e89391b8
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-26 03:08:02 -0400

    Start loop for aws

--- src/get_and_filter_vcfs.bash
+++ src/get_and_filter_vcfs.bash
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-aws s3 sync \
-    --no-sign-request \
-    --exclude="*" \
-    --include="*.hard-filtered.vcf.gz*" \
-    s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/HG00096/ .
+local_path_to_sync="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based"
+# https://questhenkart.medium.com/looping-through-an-s3-bucket-and-performing-actions-using-the-aws-cli-and-bash-61394a91af89
+origin="s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/"
+for remote_path in $(aws s3 ls --no-sign-request "${origin}");
+do
+    # Bash-ism that removes trailing slash
+    modified_remote_path=${remote_path%/}
+    # Sometimes we have text that says PRE,
+    # but we don't like that.
+    if [[ "${modified_remote_path}" != "PRE" ]];
+    then
+        # double_modified_remote_path will not have PRE
+        double_modified_remote_path="${modified_remote_path}"
 
-vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
-regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
-# Note that tabix produces something that is no longer compressed.
-tabix ${vcf} --regions ${regions} --separate-regions > "/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data/HG00096.hard-filtered.sliced.vcf"
+done
+# aws s3 sync \
+#     --no-sign-request \
+#     --exclude="*" \
+#     --include="*.hard-filtered.vcf.gz*" \
+#     s3://1000genomes-dragen/data/dragen-3.7.6/hg38-graph-based/ "${path_to_sync}"
+
+# vcf="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/data/dragen-3.7.6/hg38-graph-based/HG00096/HG00096.hard-filtered.vcf.gz"
+# regions="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom_genes.bed"
+# # Note that tabix produces something that is no longer compressed.
+# tabix ${vcf} --regions ${regions} --separate-regions > "/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data/HG00096.hard-filtered.sliced.vcf"
