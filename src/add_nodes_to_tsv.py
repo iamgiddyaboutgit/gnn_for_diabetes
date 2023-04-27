6ac084095bdc88918519e48824c994c35335b6d0
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-27 11:57:18 -0400

    loop through vcf

--- src/add_nodes_to_tsv.py
+++ src/add_nodes_to_tsv.py
@@ -16,6 +16,37 @@ def main():
     user_args = get_user_args()
     # Find the first occurrence of a line in the .vcf
     # that starts with a # and doesn't contain any tabs.
+    # loci_to_node holds the correspondences between
+    # loci and nodes.
+    loci_to_node = dict()
+    node_id = -1
+
+    with open(file=user_args.vcf, mode="r") as vcf:
+        for line in vcf:
+            # If the line starts with a # and does not
+            # have any tabs, then we need to increase
+            # the node id because whatever we find next
+            # is another section that we need.
+            if line.startswith("#") and ("\t" not in line):
+                node_id += 1
+                # This is the section for genomic loci 
+                # corresponding to the first region.
+                # Skip the header part.
+ 
+            # Check that the section is not empty.
+            elif not line.startswith("#") and ("\t" in line):
+                # line now has the info. we need in it.
+                # Get the first two fields
+                loci = "\t".join(line.split(sep="\t", maxsplit=2)[0:2])
+                loci_to_node[loci] = node_id
+
+        
+
+
+
+
+
+
 
     # Get the first two fields in following lines until
     # we reach another line that starts with # or the
@@ -31,7 +62,7 @@ def main():
     # values in the dict for each iteration.
 
     # Now, loop through the .tsv.
-    # Append "\tnode_num" to the first line.
+    # Append "\tnode_id" to the first line.
     # Write the line to the output file.
     # Go to the next line. Use the first 2 fields in the .tsv
     # to look up the value in the dict that should
@@ -53,12 +84,8 @@ def get_user_args() -> argparse.Namespace:
 
     return args
 
-def is_line_wanted():
-    """Determine if a line in a file is wanted for processing.
-    
-    The line will be wanted if it matches some REGEX.
-    """
-    pass
+
+
 
 ###############################################################################
 # if this module is being run directly and not imported
