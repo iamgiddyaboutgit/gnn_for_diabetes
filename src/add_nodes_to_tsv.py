348f935c16a6e4b28b7b15aae59d9a60a459ea6c
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-27 11:06:39 -0400

    Add pseudo-code of algorithm.
    Start writing is_line_wanted function.

--- src/add_nodes_to_tsv.py
+++ src/add_nodes_to_tsv.py
@@ -9,17 +9,34 @@ the graph. If we didn't do this, we wouldn't know which rows in the .tsv
 correspond to which nodes in the graph.
 """
 
-import sys
+import re
 import argparse
 
 def main():
     user_args = get_user_args()
     # Find the first occurrence of a line in the .vcf
     # that starts with a # and doesn't contain any tabs.
-    
+
     # Get the first two fields in following lines until
     # we reach another line that starts with # or the
-    # end of the file.
+    # end of the file.  These two fields should be 
+    # placed as new keys in a dict.  The corresponding
+    # values in the dict should be 0.
+
+    # If we reach another line that starts with a #,
+    # skip it.  On the other hand, if we reach the 
+    # end of the file, end.
+
+    # Repeat earlier stuff, storing successively higher
+    # values in the dict for each iteration.
+
+    # Now, loop through the .tsv.
+    # Append "\tnode_num" to the first line.
+    # Write the line to the output file.
+    # Go to the next line. Use the first 2 fields in the .tsv
+    # to look up the value in the dict that should
+    # be added as a new column.  Append the value to the line.
+    # Write the line to the output file.
 
 def get_user_args() -> argparse.Namespace:
     """Get arguments from the command line and validate them."""
@@ -35,6 +52,13 @@ def get_user_args() -> argparse.Namespace:
     args = parser.parse_args()
 
     return args
+
+def is_line_wanted():
+    """Determine if a line in a file is wanted for processing.
+    
+    The line will be wanted if it matches some REGEX.
+    """
+    pass
 
 ###############################################################################
 # if this module is being run directly and not imported
