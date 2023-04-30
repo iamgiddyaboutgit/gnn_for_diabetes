b494178b6ecc20e8c7e46e33aad92eb47e886348
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-30 18:31:02 -0400

    Combine info from other files into one BED.
    To make things easy on ourselves, we are going to ignore a lot of the regulation stuff and just do a 5000 bp buffer.

--- src/create_custom_bed.py
+++ src/create_custom_bed.py
@@ -4,38 +4,11 @@ import polars as pl
 
 LOCI_BUFFER_SIZE = 5000
 
-mitocarta = pl.read_csv(source="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/human_gene_id_from_mito_carta3.0_ensembl_mapping.txt",
-            has_header=True,
-            separator="\t"
-)
-
-# Replace MT with M
-fosiwiki = (pl
-            .read_csv(source="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/GenomeLoci_MITOMAP_Foswiki_ensembl_mapping.txt",
-                       has_header=True,
-                       separator="\t"
-            )
-            .with_columns(pl.lit("M").alias("chromosome_or_scaffold_name"))
-)
-
-mitocarta_and_fosiwiki = (mitocarta 
-    .vstack(fosiwiki)
-)
-
-# mitocarta_and_fosiwiki_gene_stable_ids = (mitocarta 
-#     .vstack(fosiwiki)
-#     .select("gene_stable_id")
-# )
-
-# (mitocarta_and_fosiwiki_gene_stable_ids
-#     .write_csv(file="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/mitocarta_and_fosiwiki_gene_stable_ids.txt",
-#                has_header=False,
-#                separator="\n")
-# )
-
-
-bed = (mitocarta 
-    .vstack(fosiwiki)
+mitocarta = (pl
+    .read_csv(source="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/human_gene_id_from_mito_carta3.0_ensembl_mapping.txt",
+        has_header=True,
+        separator="\t"
+    )
     .filter(
         pl.col("chromosome_or_scaffold_name")
             .str.starts_with("C")
@@ -53,8 +26,21 @@ bed = (mitocarta
 
         pl.col("chromosome_or_scaffold_name").str.replace("^", "chr").alias("chromosome_or_scaffold_name")
     ])
+)
+
+# Replace MT with chrM
+fosiwiki = (pl
+            .read_csv(source="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/GenomeLoci_MITOMAP_Foswiki_ensembl_mapping.txt",
+                       has_header=True,
+                       separator="\t"
+            )
+            .with_columns(pl.lit("chrM").alias("chromosome_or_scaffold_name"))
+)
+
+bed = (mitocarta 
+    .vstack(fosiwiki)
     .select(["chromosome_or_scaffold_name", "gene_start", "gene_end"])
-    .vstack(mt_stuff)
+    .sort(by=["chromosome_or_scaffold_name", "gene_start", "gene_end"])
 )
 
 bed.write_csv(file="/home/jpatterson87/for_classes/gnn_for_diabetes/data/raw_data/pathway_data/custom.bed",
