b0b5af86c6a98aacab201219820a14070a9e4ff4
farhibahsan <farhib.ahsan@gmail.com>
2023-05-01 10:39:35 -0400

    Continue work on training

--- src/gnn/model/GraphConvNet.py
+++ src/gnn/model/GraphConvNet.py
@@ -5,24 +5,21 @@ from torch.nn import Linear
 import torch
 
 class GNN(torch.nn.Module):
-    def __init__(self, input_size, hidden_size_1, hidden_size_2, hidden_size_3, output_size):
+    def __init__(self, input_size, hidden_size_1, hidden_size_2, hidden_size_3, hidden_size_4, output_size):
         super(GNN, self).__init__()
-        hidden_size = hidden_size_1
-        hidden_size_2 = hidden_size_2
-        hidden_size_3 = hidden_size_3
-        self.conv1 = GraphConv(input_size, hidden_size)
-        self.conv2 = GraphConv(hidden_size, hidden_size_2)
+        self.conv1 = GraphConv(input_size, hidden_size_1)
+        self.conv2 = GraphConv(hidden_size_1, hidden_size_2)
         self.conv3 = GraphConv(hidden_size_2, hidden_size_3)
-        self.fc1 = Linear(hidden_size_3, hidden_size)
-        self.fc2 = Linear(hidden_size, output_size)
+        self.fc1 = Linear(hidden_size_3, hidden_size_4)
+        self.fc2 = Linear(hidden_size_4, output_size)
 
-    def forward(self, x, edge_index, batch):
+    def forward(self, x, edge_index, edge_attr, batch):
         # TODO: Finish this function
-        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv1(x, edge_index, edge_attr))
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index, edge_attr))
         x = F.dropout(x, training=self.training)
-        x = F.relu(self.conv3(x, edge_index))
+        x = F.relu(self.conv3(x, edge_index, edge_attr))
         # x = F.dropout(x, training=self.training)
         x = global_mean_pool(x, batch)
         x = self.fc1(x)
