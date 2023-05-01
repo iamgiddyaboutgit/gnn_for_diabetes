6aaa55a74e14b4fc6a6359ad3c2aae531bea9f64
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-05-01 01:29:01 -0400

    cut tsvs, not vcfs

--- src/vcf_to_tsv.bash
+++ src/vcf_to_tsv.bash
@@ -7,12 +7,15 @@ tsv_input="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data
 src_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/src"
 output_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data_from_vms/with_nodes"
 
-# find "${vcf_input}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
-#     -exec gatk VariantsToTable --variant {} --output {}.tsv \;
+find "${vcf_input}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
+    -exec gatk VariantsToTable --variant {} --output {}.tsv \;
 
-for vcf in ${vcf_input}/*.vcf; do 
-    cut -f 1,2,4,5 "${vcf}" > "${vcf}.cut"
+# https://stackoverflow.com/questions/20796200/how-to-loop-over-files-in-directory-and-change-path-and-add-suffix-to-filename
+shopt -s nullglob
+for tsv in ${tsv_input}/*.tsv; do 
+    cut -f 1,2,4,5 "${tsv}" > "${tsv}.cut"
 done
+shopt -u nullglob
 
 "${src_dir}"/add_nodes_to_tsv.py \
     --tsv "${tsv_input}" \
