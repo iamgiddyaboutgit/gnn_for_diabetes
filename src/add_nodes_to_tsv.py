35fafb8bdd21bf3df976210291d7c30a25cd3924
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-05-01 00:57:19 -0400

    Make it work with multiple files.

--- src/add_nodes_to_tsv.py
+++ src/add_nodes_to_tsv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Add a column to a .tsv file.
+"""Add a column to some .tsv files.
 
 Given a .tsv version of a .vcf as well as a .bed, add a column to the 
 .tsv based on the info. in the .bed.  The new column should have the row 
@@ -10,14 +10,26 @@ correspond to which nodes in the graph.
 """
 
 import argparse
+from pathlib import Path
 
 def main():
     user_args = get_user_args()
-    loci_to_node = get_loci_to_node_dict(vcf=user_args.vcf)
+
+    list_of_file_tuples = get_file_tuples(
+        dir_1_path=user_args.vcf,
+        dir_1_glob="*.vcf",
+        dir_2_path=user_args.tsv,
+        dir_2_glob="*.cut"
+    )
     
-    add_nodes_to_tsv(loci_to_node=loci_to_node,
-                     tsv=user_args.tsv,
-                     out=user_args.o)
+    for vcf, cut_tsv in list_of_file_tuples:
+        loci_to_node = get_loci_to_node_dict(vcf=vcf)
+    
+        add_nodes_to_tsv(loci_to_node=loci_to_node,
+            tsv=cut_tsv,
+            out=Path(user_args.o, cut_tsv.stem).with_suffix(".tsv")
+        )
+        
     return None
 
 def get_user_args() -> argparse.Namespace:
@@ -26,11 +38,11 @@ def get_user_args() -> argparse.Namespace:
     # Optional arguments are prefixed by single or double dashes.
     # The remaining arguments are positional.
     parser.add_argument("--tsv", required = True,
-        help=".tsv file to modify")
+        help="path containnig .tsv files to modify (ending in .cut)")
     parser.add_argument("--vcf", required = True,
-        help="an uncompressed .vcf file that has already been grouped by the regions in a sorted .bed file using tabix")
+        help="path containing uncompressed .vcf files that have already been grouped by the regions in a sorted .bed file using tabix")
     parser.add_argument("-o", required=True,
-        help="Output path for new .tsv file")
+        help="Output path for new .tsv files")
     args = parser.parse_args()
 
     return args
@@ -70,6 +82,7 @@ def get_loci_to_node_dict(vcf):
 def add_nodes_to_tsv(loci_to_node, tsv, out):  
     # Loop through the .tsv, figure out what's in there,
     # and then write to out accordingly.
+    print(out)
     with open(file=tsv, mode="r") as tsv:
         with open(file=out, mode="w") as out:
             tsv_first_line = tsv.readline()
@@ -89,6 +102,32 @@ def add_nodes_to_tsv(loci_to_node, tsv, out):
                   
     return None
 
+
+def get_file_tuples(dir_1_path, dir_1_glob, dir_2_path, dir_2_glob):
+    list_of_file_tuples = []
+
+    dir_1_pure_path = Path(dir_1_path)
+    dir_2_pure_path = Path(dir_2_path)
+    
+    files_in_dir_1 = dir_1_pure_path.glob(dir_1_glob)
+    files_in_dir_2 = dir_2_pure_path.glob(dir_2_glob)
+
+    # if len(files_in_dir_1) != len(files_in_dir_2):
+    #     raise IOError
+    files_in_dir_1_names = []
+    files_in_dir_2_names = []
+    for f1 in files_in_dir_1:
+        files_in_dir_1_names.append(f1.name)
+    for f2 in files_in_dir_2:
+        files_in_dir_2_names.append(f2.name)
+    
+
+    for f1_name in files_in_dir_1_names:
+        for f2_name in files_in_dir_2_names:
+            if f1_name in f2_name:
+                list_of_file_tuples.append((dir_1_pure_path / f1_name, dir_2_pure_path / f2_name))
+    
+    return list_of_file_tuples
 
 ###############################################################################
 # if this module is being run directly and not imported
