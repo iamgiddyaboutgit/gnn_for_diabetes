ae47962098694ab9853bbc498399ce955336b555
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-30 21:32:56 -0400

    Make it work with multiple files in a loop.

--- src/vcf_to_tsv.bash
+++ src/vcf_to_tsv.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-# $1 is vcf input path
-# $2 is tsv output path
-gatk VariantsToTable \
-    --variant "${1}" \
-    --output "${2}" 
+# $1 is vcf input path prefix without a final /
+# $2 is tsv output path prefix without a final /
+
+vcf_paths=$(find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" -print0)
+
+for vcf_path in "${vcf_paths}";
+do
+    tsv_name=$(basename -s ".vcf" "${vcf_path}")
+
+    # gatk VariantsToTable \
+    #     --variant "${vcf_path}" \
+    #     --output "${2}/${tsv_name}" 
+done
