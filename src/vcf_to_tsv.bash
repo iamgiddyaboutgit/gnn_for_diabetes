9ef14c031b875f6fccf6de7d896fa666413e0213
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-30 21:55:11 -0400

    Try find -exec instead of for loop.

--- src/vcf_to_tsv.bash
+++ src/vcf_to_tsv.bash
@@ -2,13 +2,21 @@
 # $1 is vcf input path prefix without a final /
 # $2 is tsv output path prefix without a final /
 
-vcf_paths=$(find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" -print0)
+# https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
+# https://unix.stackexchange.com/questions/12902/how-to-run-find-exec
 
-for vcf_path in "${vcf_paths}";
-do
-    tsv_name=$(basename -s ".vcf" "${vcf_path}")
+find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
+    -exec gatk VariantsToTable --variant {} --output {}.tsv \;
 
-    # gatk VariantsToTable \
-    #     --variant "${vcf_path}" \
-    #     --output "${2}/${tsv_name}" 
-done
+
+# echo "${#vcf_paths[@]}"
+# for vcf_path in "${vcf_paths[@]}";
+# do
+#     tsv_name=$(basename -s ".gvcf" "${vcf_path}")
+
+#     echo "vcf_path: ${vcf_path}"
+#     echo "tsv: ${2}/${tsv_name}"
+#     # gatk VariantsToTable \
+#     #     --variant "${vcf_path}" \
+#     #     --output "${2}/${tsv_name}" 
+# done
