4e358df512c5f3b6c2222b2986f24ee78f478bc2
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-04-30 23:06:40 -0400

    Pseudocode of what needs to be done.
    The multiple statements in the exec part of find need to be broken off.

--- src/vcf_to_tsv.bash
+++ src/vcf_to_tsv.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # $1 is vcf input path prefix without a final /
-# $2 is tsv output path prefix without a final /
 
 # https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
 # https://unix.stackexchange.com/questions/12902/how-to-run-find-exec
+src_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/src"
+output_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data_from_vms/with_nodes"
 
 find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
-    -exec gatk VariantsToTable --variant {} --output {}.tsv \;
+    -exec gatk VariantsToTable --variant {} --output {}.tsv \
+    && cut -f 1,2,4,5 {}.tsv > {}.tsv.cut \
+    && "${src_dir}"/add_nodes_to_tsv.py --tsv {}.tsv.cut --vcf {} -o "${output_dir}"/{}.tsv.cut.nodes \;
+
 
 
 # echo "${#vcf_paths[@]}"
