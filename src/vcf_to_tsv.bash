9f64f9e24a20c4b16f6735ae8fafe2b5cdebde01
Justin Patterson <justinwitherlypatterson@gmail.com>
2023-05-01 01:16:32 -0400

    Improve cut.
    Next: add_nodes_to_tsv.py had a key error.

--- src/vcf_to_tsv.bash
+++ src/vcf_to_tsv.bash
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
-# $1 is vcf input path prefix without a final /
 
 # https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
 # https://unix.stackexchange.com/questions/12902/how-to-run-find-exec
+vcf_input="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data_from_vms"
+tsv_input="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data_from_vms"
 src_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/src"
 output_dir="/home/jpatterson87/for_classes/gnn_for_diabetes/data/transformed_data_from_vms/with_nodes"
 
-find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
-    -exec gatk VariantsToTable --variant {} --output {}.tsv \;
-    
-find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.tsv" \
-    -exec cut -f 1,2,4,5 {} > {}.tsv.cut \;
+# find "${vcf_input}" -maxdepth 1 -mindepth 1 -type f -name "*.vcf" \
+#     -exec gatk VariantsToTable --variant {} --output {}.tsv \;
 
-# find "${1}" -maxdepth 1 -mindepth 1 -type f -name "*.cut" \
-#     -exec "${src_dir}"/add_nodes_to_tsv.py --tsv {} --vcf {} -o "${output_dir}"/{}.tsv.cut.nodes \;
+for vcf in ${vcf_input}/*.vcf; do 
+    cut -f 1,2,4,5 "${vcf}" > "${vcf}.cut"
+done
 
-
-
-
-# echo "${#vcf_paths[@]}"
-# for vcf_path in "${vcf_paths[@]}";
-# do
-#     tsv_name=$(basename -s ".gvcf" "${vcf_path}")
-
-#     echo "vcf_path: ${vcf_path}"
-#     echo "tsv: ${2}/${tsv_name}"
-#     # gatk VariantsToTable \
-#     #     --variant "${vcf_path}" \
-#     #     --output "${2}/${tsv_name}" 
-# done
+"${src_dir}"/add_nodes_to_tsv.py \
+    --tsv "${tsv_input}" \
+    --vcf "${vcf_input}" \
+    -o "${output_dir}"
